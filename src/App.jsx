17cd215375d7ac1a8a3e1be7072ab4794645c821
Ranveer Rathour <ranveerrathour08@gmail.com>
2026-03-02 10:14:21 +0530

    Remove unused curriculum files, clean up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
 import MasterCurriculum from "./master-curriculum";
-
-// Import other curriculum components if you want to switch between them
-// import AIFullCurriculum from "./ai-full-curriculum";
 
 export default function App() {
   return <MasterCurriculum />;
